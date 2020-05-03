eb4139737a3ca1790351408c4ef457f38d12130f
TAYARNA <63741391+TAYARNA@users.noreply.github.com>
2020-05-03 14:44:57 -0300

    Update funcoes.js

--- Front3/funcoes.js
+++ Front3/funcoes.js
@@ -1,81 +1,56 @@
-function apagaMsg(){
-    document.getElementById("erroMsg").innerHTML = "";  
+function logout(){
+    localStorage.removeItem("user");
+    window.location = "Login.html";
 }
+var fragmentoPerfil = '<h3> {{NOME}} </h3>' + 
+                      '<hr>'+
+                      'RACF: {{RACF}} <br>' +
+                      'Email: {{EMAIL}} <br>' +
+                      'Setor: {{SETOR}} ' +
+                      '<hr>';
+var fragmentoFoto   = '<img src="{{LINKFOTO}}" width="100%">';
+var fragmentoPedido = '<div class="col-12">'+
+                        ' <a href="detalhepedido.html?num={{NUMPEDIDO}}"> {{DATAPEDIDO}} {{OBSERVACOES}}</a>'+
+                    '</div>';
 
-// vou criar uma funcao para autenticar (chamada pelo botao)
-function autenticaRacf(){
-    // recupero os valores digitados nos campos de INPUT
-    var login = document.getElementById("txtLogin").value;
-    var senha = document.getElementById("txtSenha").value;
 
-    // monto um Objeto JSON com esses dados (para enviar para o BAckend JAVA)
-    var msgBody = {
-         racf: login,
-         senha: senha
-    }
-    console.log(msgBody);
-
-    // tambem preciso montar um cabecalho para indicar o metodo, o corpo e os headers
-    var cabecalho = {
-        method: 'POST',
-        body: JSON.stringify(msgBody),
-        headers: {
-                'Content-Type': 'application/json'
-        }
-    }
-    var usuario;
-
-    // AJAX - Asynchronous Javascript and XML
-    // vou invocar o BackEnd atraves do FETCH
-    // ele recebe 2 parametros (o endpoint e o cabecalho) 
-    // se eu nao passar cabecalho - o padrao do FETCH eh requisicao GET
-    fetch("http://localhost:8080/login/racf",cabecalho)
-        .then(res => res.json() )  // se deu certo, pego a resposta e converto para JSON
-        .then(res => {             // se deu certo a conversao, armazendo o objeto (em forma de STRING no LocalStorage)
-            usuario = res;         // localStorage eh um tipo de "banco de dados" dentro do browser (funciona como um HASH)
-            console.log(usuario);
-            localStorage.setItem("user",JSON.stringify(usuario));
-            window.location = "profile.html"; // ai redireciono para a pagina de perfil   
-        })
-        .catch(err=>{          // se deu erro (usuario invalido), uso CSS para mostrar a mensagem de erro
-            console.log(err);
-            document.getElementById("erroMsg").innerHTML="Usuario ou senha INVALIDOS";
-        });
+function novoPedido(){
+    window.location = "novopedido.html";
 }
-
-
-function autenticaEmail(){
-    // recupero os valores digitados nos campos de INPUT
-    var login = document.getElementById("txtLogin").value;
-    var senha = document.getElementById("txtSenha").value;
-
-    // monto um Objeto JSON com esses dados (para enviar para o BAckend JAVA)
-    var msgBody = {
-         email: login,
-         senha: senha
+// este metodo eh um dos mais trabalhosos, pois ele pega a informacao do usuario
+// e tem que preencher praticamente a pagina toda.
+function carregaUser(){
+    var userStr = localStorage.getItem("user");
+    if (!userStr){  // se nao tiver isso no localStorage, redireciona para o index (login)
+        window.location = "Login.html";
     }
-    console.log(msgBody);
+    else{
 
-    // tambem preciso montar um cabecalho para indicar o metodo, o corpo e os headers
-    var cabecalho = {
-        method: 'POST',
-        body: JSON.stringify(msgBody),
-        headers: {
-                'Content-Type': 'application/json'
-        }
+        // se o usuario existe armazenado, eu pego, converto-o para JSON
+        var user = JSON.parse(userStr);
+        // e comeco a preencher as diferentes secoes da minha pagina
+        // secao do perfil
+        var strPerfil = fragmentoPerfil.replace("{{NOME}}",user.nome)
+                                       .replace("{{EMAIL}}",user.email)
+                                       .replace("{{RACF}}",user.racf)
+                                       .replace("{{SETOR}}",user.setor);
+        document.getElementById("perfil").innerHTML = strPerfil;
+
+        // secao da foto
+        document.getElementById("fotoUser").innerHTML = 
+            fragmentoFoto.replace("{{LINKFOTO}}",user.linkFoto);
+
+        // secao dos pedidos
+        var strPedidos="";
+        //console.log(user.pedidos.length);
+        //if(user.pedidos.length() > 1){
+            for (i=0; i<user.pedidos.length; i++){
+                let pedidoatual = fragmentoPedido;
+                strPedidos += pedidoatual.replace("{{DATAPEDIDO}}",user.pedidos[i].dataPedido)
+                                        .replace("{{NUMPEDIDO}}",user.pedidos[i].numPedido)   
+                                        .replace("{{OBSERVACOES}}",user.pedidos[i].observacoes);
+            }
+            document.getElementById("pedidos").innerHTML = strPedidos;
+        //}
     }
-    var usuario;
-
-    fetch("http://localhost:8080/login",cabecalho)
-        .then(res => res.json() )  // se deu certo, pego a resposta e converto para JSON
-        .then(res => {             // se deu certo a conversao, armazendo o objeto (em forma de STRING no LocalStorage)
-            usuario = res;         // localStorage eh um tipo de "banco de dados" dentro do browser (funciona como um HASH)
-            console.log(usuario);
-            localStorage.setItem("user",JSON.stringify(usuario));
-            window.location = "profile.html"; // ai redireciono para a pagina de perfil   
-        })
-        .catch(err=>{          // se deu erro (usuario invalido), uso CSS para mostrar a mensagem de erro
-            console.log(err);
-            document.getElementById("erroMsg").innerHTML="Usuario ou senha INVALIDOS";
-        });
 }
